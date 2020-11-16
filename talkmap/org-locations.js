b7d8ba8d47cad21149e10558f7cdb32d3dbf9d81
seshnadathur <39797132+seshnadathur@users.noreply.github.com>
2020-11-16 14:18:49 +0000

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,7 +1,102 @@
 var addressPoints = [
   [
+    "Pevensey 2, Falmer",
+    50.86532255,
+    -0.08692531274182405
+  ],
+  [
+    "20A Datun Road, Chaoyang District, Beijing, China",
+    40.0010629,
+    116.3948212
+  ],
+  [
+    "National Centre for Radio Astrophysics, Pune",
+    18.561263500000003,
+    73.82730742790449
+  ],
+  [
+    "Peyton Hall, Princeton",
+    40.3465523,
+    -74.65169100300668
+  ],
+  [
+    "University of Cape Town",
+    -33.95675585,
+    18.467577702002977
+  ],
+  [
+    "Rue Jean-Dominique Cassini, 91440 Bures-sur-Yvette, France",
+    48.6980604,
+    2.1721694
+  ],
+  [
+    "Royal Observatory Edinburgh",
+    55.9228827,
+    -3.188193736550935
+  ],
+  [
+    "QMUL",
+    51.5247272,
+    -0.03931034663016243
+  ],
+  [
+    "AIMS, Cape Town",
+    -34.1070816,
+    18.4705915
+  ],
+  [
+    "Philosophenweg 16, Heidelberg",
+    49.41486885,
+    8.696709553306501
+  ],
+  [
+    "University of Hull",
+    53.7710908,
+    -0.3683905962694075
+  ],
+  [
+    "Sexten, Italy",
+    46.7027185,
+    12.349573
+  ],
+  [
+    "University of Waterloo",
+    43.4701994,
+    -80.54524293779451
+  ],
+  [
     "Maynooth University, Ireland",
     53.3845884,
     -6.6007526
+  ],
+  [
+    "Via Alfonso Corti, 12, Milan",
+    45.4805862,
+    9.2316977
+  ],
+  [
+    "University of Toronto",
+    43.663461999999996,
+    -79.39775965337452
+  ],
+  [
+    "University of the Western Cape",
+    -33.93273025,
+    18.62864004583909
+  ],
+  [
+    "University of Lancaster",
+    54.00975365,
+    -2.787572601053264
+  ],
+  [
+    "University College London",
+    51.52420305,
+    -0.13300072425929543
+  ],
+  [
+    "Barcelona, Spain",
+    41.3828939,
+    2.1774322
   ]
 ];
